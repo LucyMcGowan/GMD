15efe825dfa20a441d3d05e479092a69679ac1da
Lucy <ld.mcgowan@vanderbilt.edu>
2017-02-02 17:03:11 -0600

    functions & update description

---
+++ R/src.R
@@ -0,0 +1,23 @@
+api_url <- "https://www.googleapis.com/drive/v2/files/"
+auth <- gs_auth()
+
+.api_fetch <- function(api_url, auth) {
+  req <- httr::GET(url = api_url,
+                   auth)
+
+  if (req$status_code >= 400) {
+    stop(
+      sprintf(
+        "Google API returned an error: HTTP status code %s, %s",
+        req$status_code,
+        req$headers$statusmessage
+      )
+    )
+  }
+  httr::stop_for_status(req)
+  reqlist <- httr::content(req, "parsed")
+  if (length(reqlist) == 0) stop("Zero records match your filter. Nothing to return.\n")
+
+  return(reqlist)
+}
+
